6f4d7c790df78ada57bb45efa5c4880c79c4c5ec
Aileen J Pierce <aileen.pierce@colorado.edu>
2012-09-05 20:50:38 -0600

    commit 1

---
+++ gittest3/gittest3/MADAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MADAppDelegate.h
+//  gittest3
+//
+//  Created by Aileen Pierce on 9/5/12.
+//  Copyright (c) 2012 Aileen Pierce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MADViewController;
+
+@interface MADAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MADViewController *viewController;
+
+@end
---
+++ gittest3/gittest3/MADViewController.h
@@ -0,0 +1,13 @@
+//
+//  MADViewController.h
+//  gittest3
+//
+//  Created by Aileen Pierce on 9/5/12.
+//  Copyright (c) 2012 Aileen Pierce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+
+@end
