c59698494d0ba956350966d4e23120088e6efec2
Aileen J Pierce <aileen.pierce@colorado.edu>
2012-09-10 13:01:13 -0600

    Week 3 helloworldimage

---
+++ HelloWorldImage/HelloWorldImage/BIDViewController.h
@@ -0,0 +1,17 @@
+//
+//  BIDViewController.h
+//  HelloWorldImage
+//
+//  Created by Aileen Pierce on 8/17/12.
+//  Copyright (c) 2012 Aileen Pierce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *NYImage;
+
+- (IBAction)pickImage:(id)sender;
+
+
+@end
---
+++ HelloWorldImage/HelloWorldImage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorldImage
+//
+//  Created by Aileen Pierce on 8/17/12.
+//  Copyright (c) 2012 Aileen Pierce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
