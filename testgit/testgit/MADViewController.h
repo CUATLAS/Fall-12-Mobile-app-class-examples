effb8cf8cc3978cd8f12e68fb69bdc63df59ea39
Aileen J Pierce <aileen.pierce@colorado.edu>
2012-09-13 12:05:27 -0600

    net test

---
+++ testgit/testgit/MADViewController.h
@@ -0,0 +1,13 @@
+//
+//  MADViewController.h
+//  testgit
+//
+//  Created by Aileen Pierce on 9/13/12.
+//  Copyright (c) 2012 Aileen Pierce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+
+@end
